ce2105f5bb963687ce3cc4f67fbec8ad2b31b6d5
L-Ward <noreply>
2018-06-26 19:11:33 -0700

    Working word generator, hidden word display, and functionality for key inputs.

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+// variables
+var words = ["mermaid", "narwhal", "sharks", "fish", "shipwreck", "water", "seaweed"];
+var hiddenWord = "";
+// var wins;
+// var numGuesses;
+// var guessed = [];
+
+
+//set currentWord
+var randomWord = words[Math.floor(Math.random() * words.length)];
+
+//Replacing string characters with _
+for (i = 0; i < randomWord.length; i++) {
+    console.log(randomWord);
+    hiddenWord += "_";
+}
+displayInfo(hiddenWord);
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function (event) {
+    // Determines which key was pressed.
+    var userGuess = event.key;
+
+    //Check if userGuess exists in randomWord string
+    var ug = randomWord.includes(userGuess);
+    if (ug === true) {
+        //may need to loop through this to see if userGuess exists multiple times
+        //If userGuess exists replace _ with userGuess
+        for (i = 0; i < randomWord.length; i++) {
+            if (randomWord[i] === userGuess) {
+                hiddenWord = hiddenWord.substring(0, i) + userGuess + hiddenWord.substring(i + 1);
+            }
+        }
+    } 
+    displayInfo(hiddenWord);
+}
+
+//Functions
+//Display function
+function displayInfo(hiddenWord) {
+    //displaying hidden word
+    var displayString = hiddenWord.split('').join(' ');
+    document.querySelector("#currentWord").innerHTML = displayString;
+}
