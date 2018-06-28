8fb1d4272c6e4222bc5da39b0ca9e9d2f78ccb86
L-Ward <noreply>
2018-06-28 08:32:50 -0700

    added win/lose screen to replace alerts.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,34 +12,37 @@ initializeGameData();
 document.onkeyup = function (event) {
     // Determines which key was pressed.
     var userGuess = event.key;
-
-    //Check if userGuess exists in randomWord string
-    var ug = randomWord.includes(userGuess);
-    if (ug === true) {
-        //If userGuess exists replace _ with userGuess
-        for (i = 0; i < randomWord.length; i++) {
-            if (randomWord[i] === userGuess) {
-                hiddenWord = hiddenWord.substring(0, i) + userGuess + hiddenWord.substring(i + 1);
-            }
-        }
-    } else {
-        //Prevent user from inputting same guess multiple times
-        if (guessed.includes(userGuess)) {
-            alert("You've already tried that one.");
+        if (event.keyCode == 32) {
+            reset();
         } else {
-            //decrement gues counter and store user input
-            numGuess--;
-            guessed.push(userGuess);
+            //Check if userGuess exists in randomWord string
+            var ug = randomWord.includes(userGuess);
+            if (ug === true) {
+                //If userGuess exists replace _ with userGuess
+                for (i = 0; i < randomWord.length; i++) {
+                    if (randomWord[i] === userGuess) {
+                        hiddenWord = hiddenWord.substring(0, i) + userGuess + hiddenWord.substring(i + 1);
+                    }
+                }
+            } else {
+                //Prevent user from inputting same guess multiple times
+                if (guessed.includes(userGuess)) {
+                    alert("You've already tried that one.");
+                } else {
+                    //decrement guess counter and store user input
+                    numGuess--;
+                    guessed.push(userGuess);
+                }
+            }
+            displayInfo(hiddenWord, numGuess, guessed);
+            checkWinConditions();
         }
     }
-    displayInfo(hiddenWord, numGuess, guessed);
-    checkWinConditions();
-}
 
 //Functions
 //Display function
 function displayInfo(hiddenWord, numGuess, guessed) {
-    //displaying hidden word
+    //displaying hidden wordwith spaces between underscores
     var displayString = hiddenWord.split('').join(' ');
     document.querySelector("#currentWord").innerHTML = displayString;
     document.querySelector("#numGuesses").innerHTML = numGuess;
@@ -50,13 +53,11 @@ function displayInfo(hiddenWord, numGuess, guessed) {
 function initializeGameData() {
     //set currentWord
     randomWord = words[Math.floor(Math.random() * words.length)];
-
     //Replacing string characters with _
     for (i = 0; i < randomWord.length; i++) {
         console.log(randomWord);
         hiddenWord += "_";
     }
-
     displayInfo(hiddenWord, numGuess, guessed);
 }
 
@@ -65,11 +66,9 @@ function checkWinConditions() {
         wins++;
         var audio = document.getElementById("soundClip");
         audio.play();
-        alert("You win!");
-        reset();
+        document.getElementById("winFlag").style.display = "block";
     } else if (numGuess <= 0) {
-        alert("You lose.");
-        reset();
+        document.getElementById("lostFlag").style.display = "block";
     }
 }
 
@@ -80,5 +79,8 @@ function reset() {
     numGuess = 15;
     //reset hidden word
     hiddenWord = "";
+    //hide win or lose flags
+    document.getElementById("winFlag").style.display = "none";
+    document.getElementById("lostFlag").style.display = "none";
     initializeGameData()
 }
